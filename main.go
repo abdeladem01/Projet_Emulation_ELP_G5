2088eb92ac90e3b14e8c28369fc306798ee1e012
Abdeladem Fattah <74178270+abdeladem01@users.noreply.github.com>
2021-10-20 14:39:38 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+  "time"
+  "strconv"
+)
+func genavion(n int){
+  var l []string
+  var a int 
+  var b int
+  var k int
+  var p int
+  for i:=0;i<n;i++{
+    var lu []string
+    for i:=0;i<2;i++{
+      rand.Seed(time.Now().UnixNano())
+      min:=0
+      max:=500
+      a:=(rand.Intn(max - min + 1) + min)
+      b:=(rand.Intn(max -min +1) + min)
+      k := strconv.Itoa(a)
+      p:= strconv.Itoa(b)
+      
+    }
+  l.append(lu)
+  }
+  fmt.Println(l)
+}
+func main() {
+  genavion(5)
+}
